e242ff5fabc78a054602ff53331d1e4f61d807c2
Levente <eager-hun@users.noreply.github.com>
2016-06-26 23:03:37 +0200

    Added .container wrapper around the generator-ui.

--- engineroom/app/generator-ui.php
+++ engineroom/app/generator-ui.php
@@ -1,15 +1,18 @@
 
+<div class="container">
 
-<!--input id="html-generator-input-dirname" type="text" -->
+  <!--input id="html-generator-input-dirname" type="text" -->
 
-<button
-  id="html-generator-button"
-  class="button html-generator-button"
-  type="button">
-    Generate
-</button>
+  <button
+    id="html-generator-button"
+    class="button html-generator-button"
+    type="button">
+      Generate
+  </button>
 
-<div>Responses:</div>
+  <div>Responses:</div>
 
-<div id="app-ajax-messages" class="app-ajax-messages"></div>
+  <div id="app-ajax-messages" class="app-ajax-messages"></div>
+
+</div>
 
