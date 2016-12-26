bbfadab2e8b17d3a7500ac9f721664210432f02c
Levente <eager-hun@users.noreply.github.com>
2016-12-27 00:19:00 +0100

    Included README.md files' content on the homepage.

--- anypages/definitions/page-prescriptions/home.prescription.php
+++ anypages/definitions/page-prescriptions/home.prescription.php
@@ -4,6 +4,15 @@ use \Michelf\Markdown;
 
 echo $apsHelper->render_page_title('Home page');
 
-$text = "Hi, this is the home page's content.";
+$text = <<<EOT
+This website consists of two specifically developed projects: "anypage" and "theme-seed".
+
+Below are included the contents of these projects' `README.md` files.
+EOT;
+
 echo Markdown::defaultTransform($text);
+
+echo $apsHelper->import_file_content('README.md', 'md');
+
+echo $apsHelper->import_file_content('themes/theme-seed/README.md', 'md');
 
