50f2f8cc8bd79a688688728683caf832ae6c8633
Levente <eager-hun@users.noreply.github.com>
2017-12-26 01:09:34 +0100

    Refactored the "Layout-1-sidebar" demo page.
    Looks now better + now it uses the corresponding twig template (which it
    somehow forgotten to do, so far).

--- private/anypages/definitions/anypage-recipes/styles-demo-layout-1-sidebar.recipe.php
+++ private/anypages/definitions/anypage-recipes/styles-demo-layout-1-sidebar.recipe.php
@@ -1,24 +1,84 @@
 <?php
 
+$page_levels = [];
+
+
+// #############################################################################
+// Page title.
 
 $page_title = $tools->render('page-title', [
     'page_title_text' => 'Layout with 1 sidebar'
 ]);
 
-// ----------------------------------------------------------------------------
-// Demo content.
-
-$demo_content = $tools->importFileContent(
-    APS_CONTENTS . '/arbitrary/styles-demo-layout-1-sidebar.php',
-    'php'
-);
+$page_levels[] = [
+    'page_level_content' => $page_title
+];
 
 
-// ----------------------------------------------------------------------------
-// Output.
+// #############################################################################
+// Samples.
 
-$page_content = $tools->render('layouts/page-level', [
-    'page_level_content' => $page_title . $demo_content,
+$main_content = $tools->addFillerText('s', 1, true);
+
+$box_for_sidebar = $tools->render('patterns/box', [
+    'wrapper_extra_classes' => 'fill-flex color-zone color-zone--brand',
+    'box_title' => 'Sidebar content',
+    'box_content' => '<p>' . $tools->addFillerText('xxs', 1, false) . ' . </p>'
 ]);
 
-echo $page_content;
+// -----------------------------------------------------------------------------
+// Next level.
+
+$title1 = '<h2 class="underlined">Sidebar is present, on the right</h2>';
+
+$sample1 = $tools->render('layouts/layout-1-sidebar', [
+    'wrapper_extra_classes' => 'sidebar-on-right has-sidebar',
+    'main_content'          => $title1 . $main_content,
+    'sidebar_content'       => $box_for_sidebar,
+]);
+
+$page_levels[] = [
+    'page_level_content' => $sample1
+];
+
+// -----------------------------------------------------------------------------
+// Next level.
+
+$title2 = '<h2 class="underlined">Sidebar is present, on the left</h2>';
+
+$sample1 = $tools->render('layouts/layout-1-sidebar', [
+    'wrapper_extra_classes' => 'sidebar-on-left has-sidebar',
+    'main_content'          => $title2 . $main_content,
+    'sidebar_content'       => $box_for_sidebar,
+]);
+
+$page_levels[] = [
+    'page_level_content' => $sample1
+];
+
+// -----------------------------------------------------------------------------
+// Next level.
+
+$title3 = '<h2 class="underlined">Sidebar is not present</h2>';
+
+$sample1 = $tools->render('layouts/layout-1-sidebar', [
+    'main_content' => $title3 . $main_content,
+]);
+
+$page_levels[] = [
+    'page_level_content' => $sample1
+];
+
+// #############################################################################
+// Output.
+
+foreach ($page_levels as $manifest) {
+    echo $tools->render('layouts/page-level', $manifest);
+}
+
+
+
+
+
+
+
