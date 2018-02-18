34eaf3d635194746c8a7804a18ed514aa37986a8
Levente <eager-hun@users.noreply.github.com>
2018-02-18 16:28:05 +0100

    Updated systems-wiring-check page to help verify CSS background images' wiring.

--- private/anypages/definitions/anypage-recipes/meta-check-systems-wiring.recipe.php
+++ private/anypages/definitions/anypage-recipes/meta-check-systems-wiring.recipe.php
@@ -19,31 +19,92 @@ $page_levels[] = [
 
 
 // -----------------------------------------------------------------------------
-// Check static assets' wiring.
+// Check Styleguide's assets' wiring.
 
-$sa_title = '<h2 class="underlined">Static assets</h2>';
+$sg_assets_title = '<h2 class="underlined">Styleguide assets</h2>';
 
-$sa_desc_raw = "Verify intact static asset wiring by seeing an image here:";
-$sa_desc = $tools->markdown($sa_desc_raw);
+$sg_assets_desc_raw = <<<EOT
+Verify intact styleguide asset wiring by seeing nicely colourful markup here:
 
-$sa_img_src = $tools
-        ->pathToThemeStaticFiles() . '/images/for-demo/photo-1-700.jpg';
+(Note that other tests may rely on the success of this one.)
+EOT;
+$sg_assets_desc = $tools->markdown($sg_assets_desc_raw);
 
-$sa_check = <<<EOT
-    <img src="${sa_img_src}" aria-hidden="true">
+$sample_markup = <<<EOT
+<div>
+    <span class="foo bar">All the colours!</span>
+</div>
 EOT;
 
-$check_sa_layout = $tools->render('layouts/flex-grid', [
+$code_demo = $tools->render('patterns/texts/code', [
+    'code_classes' => 'language-markup',
+    'code' => $sample_markup
+]);
+
+$check_sg_assets_layout = $tools->render('layouts/flex-grid', [
     'wrapper_extra_classes' => 'flex-grid--preset-2-cols',
     'items_extra_classes'   => 'fit-content',
     'items' => [
-        [ 'item_content' => $sa_title . $sa_desc, ],
-        [ 'item_content' => $sa_check, ],
+        [ 'item_content' => $sg_assets_title . $sg_assets_desc, ],
+        [ 'item_content' => $code_demo, ],
     ],
 ]);
 
 $page_levels[] = [
-    'page_level_content' => $check_sa_layout
+    'page_level_content' => $check_sg_assets_layout
+];
+
+
+// -----------------------------------------------------------------------------
+// Check static assets' wiring when linked from HTML.
+
+$sal_title = '<h2 class="underlined">Static assets referenced in HTML</h2>';
+
+$sal_desc_raw = "Verify intact static asset wiring by seeing an image here:";
+$sal_desc = $tools->markdown($sal_desc_raw);
+
+$sal_img_src = $tools
+        ->pathToThemeStaticFiles() . '/images/for-demo/photo-1-700.jpg';
+
+$sal_check = <<<EOT
+    <img src="${sal_img_src}" alt="test-image">
+EOT;
+
+$check_sal_layout = $tools->render('layouts/flex-grid', [
+    'wrapper_extra_classes' => 'flex-grid--preset-2-cols',
+    'items_extra_classes'   => 'fit-content',
+    'items' => [
+        [ 'item_content' => $sal_title . $sal_desc, ],
+        [ 'item_content' => $sal_check, ],
+    ],
+]);
+
+$page_levels[] = [
+    'page_level_content' => $check_sal_layout
+];
+
+
+// -----------------------------------------------------------------------------
+// Check static assets' wiring when linked from CSS.
+
+$sac_title = '<h2 class="underlined">Static assets referenced in CSS</h2>';
+
+$sac_desc_raw = "Verify intact CSS asset referencing by seeing an image here:";
+$sac_desc = $tools->markdown($sac_desc_raw);
+
+$sac_check = '<div class="meta-check-systems-wiring--css-bg-image"></div>';
+
+$check_sac_layout = $tools->render('layouts/flex-grid', [
+    'wrapper_extra_classes' => 'flex-grid--preset-2-cols',
+    'items_extra_classes'   => 'fit-content',
+    'items' => [
+        [ 'item_content' => $sac_title . $sac_desc, ],
+        [ 'item_content' => $sac_check, ],
+    ],
+]);
+
+$page_levels[] = [
+    'page_level_content' => $check_sac_layout
 ];
 
 
@@ -73,39 +134,6 @@ $check_vue_layout = $tools->render('layouts/flex-grid', [
 
 $page_levels[] = [
     'page_level_content' => $check_vue_layout
-];
-
-
-// -----------------------------------------------------------------------------
-// Check Styleguide's assets' wiring.
-
-$sg_assets_title = '<h2 class="underlined">Styleguide assets</h2>';
-
-$sg_assets_desc_raw = "Verify intact styleguide asset wiring by seeing nicely colourful markup here:";
-$sg_assets_desc = $tools->markdown($sg_assets_desc_raw);
-
-$sample_markup = <<<EOT
-<div>
-    <span class="foo bar">All the colours!</span>
-</div>
-EOT;
-
-$code_demo = $tools->render('patterns/texts/code', [
-    'code_classes' => 'language-markup',
-    'code' => $sample_markup
-]);
-
-$check_sg_assets_layout = $tools->render('layouts/flex-grid', [
-    'wrapper_extra_classes' => 'flex-grid--preset-2-cols',
-    'items_extra_classes'   => 'fit-content',
-    'items' => [
-        [ 'item_content' => $sg_assets_title . $sg_assets_desc, ],
-        [ 'item_content' => $code_demo, ],
-    ],
-]);
-
-$page_levels[] = [
-    'page_level_content' => $check_sg_assets_layout
 ];
 
 
