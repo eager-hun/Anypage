fc3bd9c4c37b9c48977958c6c5a76e50ca814acb
Levente <eager-hun@users.noreply.github.com>
2018-02-19 01:06:28 +0100

    Added forms-related templates + added forms-page content to demo forms.

---
+++ private/anypages/contents/arbitrary/styles-demo-form-widgets.php
@@ -0,0 +1,202 @@
+<?php
+
+// #############################################################################
+// Preparing text-ish input types' manifest.
+
+$textish_input_types = [
+    'text',
+    'email',
+    'password',
+    'search',
+    'tel',
+    'url',
+    'number',
+    'date',
+    'month',
+    'week',
+    'time',
+    'datetime-local',
+];
+
+$textish_inputs_manifest = [];
+
+foreach ($textish_input_types as $input_type) {
+    $textish_inputs_manifest[] = [
+        'label_data' => [
+            'text' => ucfirst(strtolower($input_type)),
+        ],
+        'widget_data' => [
+            'template' => 'forms/input--textish',
+            'attributes' => [
+                'type' => $input_type,
+                'id' => 'widget-inventory__input-textish--' . $input_type,
+                'class' => 'input--oneliner'
+            ],
+        ],
+        'container_data' => [
+            'helptext' => 'Help text for input type ' . $input_type
+        ],
+    ];
+}
+
+
+// #############################################################################
+// Button-ish widgets.
+
+$buttonish_widgets_manifest = [
+    [
+        'label_data' => [
+            'text' => 'Button as <button>',
+        ],
+        'widget_data' => [
+            'template' => 'forms/button',
+            'attributes' => [
+                'type' => 'submit',
+                'id' => 'widget-inventory__button-as-button',
+                'class' => 'button button--oneliner',
+            ],
+            'value' => 'Button as <button>'
+        ],
+    ],
+    [
+        'label_data' => [
+            'text' => 'Button as link',
+        ],
+        'widget_data' => [
+            'template' => 'forms/button--as-link',
+            'attributes' => [
+                'id' => 'widget-inventory__button-as-link',
+                'class' => 'button button--oneliner'
+            ],
+            'value' => 'Button as link'
+        ],
+    ]
+];
+
+
+// #############################################################################
+// Misc widgets.
+
+$misc_widgets_manifest = [
+    [
+        'label_data' => [
+            'text' => 'Textarea',
+        ],
+        'widget_data' => [
+            'template' => 'forms/textarea',
+            'attributes' => [
+                'id' => 'widget-inventory__textarea',
+                'required' => 'required',
+            ],
+            'value' => '',
+        ],
+        'container_data' => [
+            'helptext' => 'Help text for the textarea',
+        ],
+    ],
+    [
+        'label_data' => [
+            'text' => 'Checkbox',
+        ],
+        'widget_data' => [
+            'template' => 'forms/input--checkbox',
+            'attributes' => [
+                'type' => 'checkbox',
+                'id' => 'widget-inventory__checkbox',
+            ],
+        ],
+    ],
+    [
+        'label_data' => [
+            'text' => 'Radio',
+        ],
+        'widget_data' => [
+            'template' => 'forms/input--radio',
+            'attributes' => [
+                'type' => 'radio',
+                'id' => 'widget-inventory__radio',
+            ],
+        ],
+    ],
+    [
+        'label_data' => [
+            'text' => 'Select',
+        ],
+        'widget_data' => [
+            'template' => 'forms/select',
+            'attributes' => [
+                'id' => 'widget-inventory__select',
+                'class' => 'input--oneliner',
+                'placeholder' => '',
+                'foobar' => 'foobar',
+            ],
+            'options' => [
+                [ 'value' => 'foo' ],
+                [ 'value' => 'bar' ],
+                [ 'value' => 'baz' ],
+            ]
+        ],
+    ],
+    [
+        'label_data' => [
+            'text' => 'File',
+        ],
+        'widget_data' => [
+            'template' => 'forms/input--file',
+            'attributes' => [
+                'type' => 'file',
+                'id' => 'widget-inventory__file',
+            ],
+        ],
+    ],
+];
+
+
+// #############################################################################
+// Finalizing the all-widgets' manifest.
+
+$all_widgets_manifest = array_merge(
+    $textish_inputs_manifest,
+    $misc_widgets_manifest,
+    $buttonish_widgets_manifest
+);
+
+
+// #############################################################################
+// Rendering widgets.
+
+$rendered_widgets_inventory = '';
+
+foreach ($all_widgets_manifest as $widget_manifest) {
+
+    $rendered_widget = $tools->render(
+        $widget_manifest['widget_data']['template'],
+        $widget_manifest['widget_data']
+    );
+
+    $rendered_label = $tools->render(
+        'forms/label--as-sibling',
+        [
+            'extra_classes' => 'label--above',
+            'widget_id' => $widget_manifest['widget_data']['attributes']['id'],
+            'text' => $widget_manifest['label_data']['text'],
+        ]
+    );
+
+    $widget_container_data = [
+        'label' => $rendered_label,
+        'widget' => $rendered_widget,
+    ];
+
+    if ( ! empty($widget_manifest['container_data']['helptext'])) {
+        $widget_container_data['helptext'] =
+            $widget_manifest['container_data']['helptext'];
+    }
+
+    $rendered_widgets_inventory .= $tools->render(
+        'forms/widget-container',
+        $widget_container_data
+    );
+}
+
+echo "<form action='#'>$rendered_widgets_inventory</form>";
--- private/anypages/definitions/anypage-recipes/styles-demo-forms.recipe.php
+++ private/anypages/definitions/anypage-recipes/styles-demo-forms.recipe.php
@@ -1,5 +1,51 @@
 <?php
 
-$page_content = 'Forms demo.';
+$page_levels = [];
 
-echo $page_content;
+
+// -----------------------------------------------------------------------------
+// Page title.
+
+$page_levels[] = [
+    'page_level_content' => $tools->render('page/page-title', [
+        'page_title_text' => "Forms"
+    ]),
+];
+
+
+// -----------------------------------------------------------------------------
+// Inventory of widgets.
+
+$widgets_inventory_title = '<h2 class="underlined">Widgets inventory</h2>';
+
+$widgets_inventory = $tools->importFileContent(
+    APS_CONTENTS . '/arbitrary/styles-demo-form-widgets.php',
+    'php'
+);
+
+$page_levels[] = [
+    'page_level_content' => $widgets_inventory_title . $widgets_inventory
+];
+
+
+// -----------------------------------------------------------------------------
+// Freetext tinkering.
+
+$freetext_tinkering_title = '<h2 class="underlined">Freetext tinkering</h2>';
+
+$freetext_tinkering = $tools->importFileContent(
+    APS_CONTENTS . '/arbitrary/tinkering-forms-freetext.php',
+    'php'
+);
+
+$page_levels[] = [
+    'page_level_content' => $freetext_tinkering_title . $freetext_tinkering
+];
+
+
+// ----------------------------------------------------------------------------
+// Output.
+
+foreach ($page_levels as $manifest) {
+    echo $tools->render('layouts/page-level', $manifest);
+}
