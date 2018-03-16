6dd904afd2441dc670ce093f1e9731decf0cc1d9
Levente <eager-hun@users.noreply.github.com>
2018-03-16 11:54:05 +0100

    Twig templates can be introduced in the anypages' dir and in the theme, too.

--- director.php
+++ director.php
@@ -84,12 +84,28 @@ $capacities::bind('site-generator', new SiteGenerator(
 ));
 
 // TWIG.
-// See http://twig.sensiolabs.org/doc/api.html#environment-options .
+// See https://twig.symfony.com/doc/2.x/api.html#environment-options
+// See https://twig.symfony.com/doc/2.x/api.html#built-in-loaders
 
 $templating_config = $processManager->getConfig('config')['app']['templating'];
 
 if ( ! empty($templating_config['enable-twig'])) {
     $twig_loader = new Twig_Loader_Filesystem(TEMPLATES);
+
+    // One can use (and override existing) templates from the anypages' dir.
+    $anypage_templates = ANYPAGES . '/templates';
+    if (file_exists($anypage_templates)) {
+        $twig_loader->prependPath($anypage_templates);
+    }
+
+    // One can use (and override existing) templates from the theme.
+    $path_to_theme = $processManager
+        ->getInstruction('path-fragment-to-theme');
+    $theme_templates = PUBLIC_RESOURCES . '/' . $path_to_theme . '/templates';
+    if (file_exists($theme_templates)) {
+        $twig_loader->prependPath($theme_templates);
+    }
+
     $twig_options = $templating_config['twig-renderer-options'];
 
     $twig = new Twig_Environment($twig_loader, $twig_options);
