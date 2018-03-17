f9f90d804fed507064bad10fa6313e7389f9878a
Levente <eager-hun@users.noreply.github.com>
2018-03-17 16:49:52 +0100

    Made the generator UI use the button styling that the theme provides.

--- private/templates/app-infra/generator-ui.tpl.php
+++ private/templates/app-infra/generator-ui.tpl.php
@@ -1,8 +1,10 @@
 
 <div class="generator__trigger">
     <p>Generate a static snapshot of the current state of the site.</p>
-    <button type="button" id="html-generator-button" class="html-generator-button">
-        Generate
+    <button type="button" id="html-generator-button" class="button button--primary html-generator-button">
+        <span class="button__text">
+            Generate
+        </span>
     </button>
 </div>
 
