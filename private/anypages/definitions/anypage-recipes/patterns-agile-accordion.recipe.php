9bd4f1d69e20c2d2b4d50c2fc2075a15c1571e8a
Levente <eager-hun@users.noreply.github.com>
2018-04-15 23:19:44 +0200

    Progress with the development of vue-agile-accordion.

--- private/anypages/definitions/anypage-recipes/patterns-agile-accordion.recipe.php
+++ private/anypages/definitions/anypage-recipes/patterns-agile-accordion.recipe.php
@@ -4,29 +4,34 @@ echo $tools->render('page/page-title', [
     'page_title_text' => 'Patterns: agile accordion'
 ]);
 
-$aaccdn_manifest = [
+$accdn_manifest = [
     'settings' => [
-        'fooSetting' => 'hoj'
+        'tabsAt' => 500,
+        'exclusiveItems' => 0, // Bool.
     ],
     'items' => [
         [
-            'title' => 'Item 1 title',
-            'content' => $tools->addFillerText('s', 1, true),
+            'title' => 'Item 0',
+            'content' => $tools->markdown('Item 0')
+                . $tools->addFillerText('s', 1, true),
+        ],
+        [
+            'title' => 'Item 1',
+            'content' => $tools->markdown('Item 1')
+                . $tools->addFillerText('s', 2, true),
             'initially_open' => true,
         ],
         [
-            'title' => 'Item 2 title',
-            'content' => $tools->addFillerText('s', 2, true),
+            'title' => 'Item 2',
+            'content' => $tools->markdown('Item 2')
+                . $tools->addFillerText('s', 3, true),
         ],
         [
-            'title' => 'Item 3 title',
-            'content' => $tools->addFillerText('s', 3, true),
+            'title' => 'Item 3',
+            'content' => $tools->markdown('Item 3')
+                . $tools->addFillerText('s', 4, true),
         ],
-        [
-            'title' => 'Item 4 title',
-            'content' => $tools->addFillerText('s', 4, true),
-        ],
-    ]
+    ],
 ];
 
-echo $tools->render('patterns/collapsibles/agile-accordion', $aaccdn_manifest);
+echo $tools->render('patterns/collapsibles/agile-accordion', $accdn_manifest);
